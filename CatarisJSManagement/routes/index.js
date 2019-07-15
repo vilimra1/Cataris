0d05cd3ce07a2dbbd3b79095d769853b513a313b
Michal Horák <michal.horak@sabris.com>
2019-07-15 12:49:06 +0200

    App management

---
+++ CatarisJSManagement/routes/index.js
@@ -0,0 +1,59 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var hdb = require('hdb');
+
+if (process.env.VCAP_SERVICES) {
+    //app is running in the cloud
+    // parse the environement variabels to get the credentials
+    var svcs = JSON.parse(process.env.VCAP_SERVICES);
+    var credentials = svcs["hanadb"][0].credentials;
+    var options = {
+        host: credentials.hostname,
+        port: credentials.port,
+        user: credentials.username,
+        password: credentials.password,
+        database: credentials.name,
+        useTLS: true
+    };
+}
+else {
+    //local setup - credentials used if app is runnig local
+    var options = {
+        host: "zeus.hana.prod.eu-central-1.whitney.dbaas.ondemand.com",
+        port: 23356,
+        user: "CATARIS_HDI_CATARISDB_1_9B3YF0APDX02RBTFU61VUGLFI_RT",
+        password: "Mw0ja9Xvht-cCWm3n0M.0dOTORWV.uRVROAa31mvMy54HQFi-H.2bpwboP9QR2VaztW7sE4BlUagO5heu7ER1YD2F3uX72LeRFftq5UoIrMq.nIoFUdh10yr24PTHj2t",
+        database: "CATARIS_HDI_CATARISDB_1",
+        useTLS: true
+    };
+}
+
+var client = hdb.createClient(options);
+client.on('error', function (err) {
+    console.error('Network connection error', err);
+});
+
+client.connect(function (err) {
+    if (err) {
+        return console.error('Error:', err);
+    }
+    console.log(client.readyState); // connected
+});
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    client.exec('SELECT TOP 1000 \"usernr\",\"username\",\"password\",\"email\", \"tel\" FROM \"CATARIS_HDI_CATARISDB_1\".\"Cataris.CatarisDB::tables.user\";', function (err, rows) {
+        if (err) {
+            return console.error('Error:', err);
+        }
+        res.send(rows);
+    });
+
+
+    //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
